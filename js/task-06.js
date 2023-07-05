45e3d32bb7fb374829dfee78bd28d1987f418860
Mikhail <99617835+mikhailantonyuk@users.noreply.github.com>
2023-07-05 10:22:47 +0300

    Update task-06.js
    task6

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,9 @@
 document.getElementById("validation-input").onblur = function() {
         console.log(this.value.length);
         if (this.getAttribute('data-length') != this.value.length) { 
+          this.classList.remove('valid');
           this.classList.add('invalid');
+          
           
         } else {
           this.classList.replace('invalid', 'valid');
